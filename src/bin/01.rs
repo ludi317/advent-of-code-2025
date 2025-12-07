33e87272e72f067d27c076ecb3b3da1552fd0a76
Ludi Rehak <ludi317@gmail.com>
2025-12-06 20:01:19 -0800

    Day 1 Part 2. Floor division in rust is div_euclid().

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,5 +1,3 @@
-use advent_of_code::get_nums;
-
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<isize> {
@@ -22,8 +20,29 @@ pub fn part_one(input: &str) -> Option<isize> {
     Some(ans)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<isize> {
+    let mut dial = 50;
+    let mut ans = 0;
+    let n = 100;
+    for line in input.lines() {
+        let (sign, number_str) = line.split_at(1);
+        let sign = match sign {
+            "L" => -1,
+            "R" => 1,
+            _ => unreachable!(),
+        };
+        let num: isize = number_str.parse().unwrap();
+        let (a, b) = if sign > 0 {
+            (dial + 1, dial + num)
+        } else {
+            (dial - num, dial - 1)
+        };
+        // count multiples of n in [a, b]
+        ans += b.div_euclid(n) - (a-1).div_euclid(n);
+        dial += sign * num;
+        dial %= 100;
+    }
+    Some(ans)
 }
 
 #[cfg(test)]
@@ -39,6 +58,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
